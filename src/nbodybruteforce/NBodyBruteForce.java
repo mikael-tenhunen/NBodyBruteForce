8f8fe07b99c28a826b875c11a2e1f9e688a542a9
Mikael Tenhunen <mikael.tenhunen@gmail.com>
2014-03-03 11:06:58 +0100

    started project

---
+++ src/nbodybruteforce/NBodyBruteForce.java
@@ -0,0 +1,15 @@
+package nbodybruteforce;
+
+/**
+ *
+ */
+public class NBodyBruteForce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
